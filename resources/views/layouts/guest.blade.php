9bac7e3e7293801f8bb104452cc827a6e78cc2c1
Brichrist <hanjayachristian@gmail.com>
2025-11-17 01:10:19 +0700

    adjust

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,6 +7,11 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- Favicon -->
+        <link rel="icon" type="image/jpeg" href="{{ asset('download.jpeg') }}">
+        <link rel="apple-touch-icon" href="{{ asset('download.jpeg') }}">
+        <link rel="shortcut icon" type="image/jpeg" href="{{ asset('download.jpeg') }}">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
